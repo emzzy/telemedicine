ac6f1200d24ab8735eb6edf10d89de3dc51212d7
emzzy <ezehemmanuel265@gmail.com>
2024-12-26 17:27:49 +0000

    created subscription model app

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
+
+SUBSCRIPTION_PERMISSIONS = [
+    ("advanced", "Advanced Perm"),
+    ("pro", "Pro Perm"),
+    ("basic", "Basic Perm")
+]
 
 # Create your models here.
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
+    groups = models.ManyToManyField(Group)
+    permissions = models.ManyToManyField(
+        Permission, 
+        limit_choices_to={
+        "content_type__app_label": "subscriptions",
+        "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS]
+    })
 
     class Meta:
-        permissions = [
-            ("advanced", "Advanced Perm"),
-            ("pro", "Pro Perm"),
-            ("basic", "Basic Perm")
-        ]
+        permissions = SUBSCRIPTION_PERMISSIONS
