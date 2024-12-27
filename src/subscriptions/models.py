96dcfdd24483489f378d5132e0d7a7ed8484c5f5
emzzy <ezehemmanuel265@gmail.com>
2024-12-27 01:27:22 +0000

    created groups for user subscriptions

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import Group, Permission
+from django.db.models.signals import post_save
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+ALLOW_CUSTOM_GROUPS = True
 
 SUBSCRIPTION_PERMISSIONS = [
     ("advanced", "Advanced Perm"),
     ("pro", "Pro Perm"),
-    ("basic", "Basic Perm")
+    ("basic", "Basic Perm"),
+    #("basic_ai", "Basic AI Perm")
 ]
 
 # Create your models here.
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
     groups = models.ManyToManyField(Group)
     permissions = models.ManyToManyField(
         Permission, 
@@ -18,5 +26,24 @@ class Subscription(models.Model):
         "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS]
     })
 
+    def __str__(self):
+        return f"{self.name}"
+
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
+
+class UserSubscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription,
+    on_delete=models.SET_NULL, null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+
+def user_sub_post_save(sender, instance, *args, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups = subscription_obj.groups.all()
+    user.groups.set(groups)
+
+post_save.connect(user_sub_post_save, sender=UserSubscription)
