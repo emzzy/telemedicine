3d45befd9ea58579d28ff920f70812fa144348f5
emzzy <ezehemmanuel265@gmail.com>
2025-01-02 18:10:08 +0000

    tests for authenticated users

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from api.models import Patient, MedicalProfessional
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+class PatientViewsTestCase(TestCase):
+
+    def test_view_patients_unauthenticated(self):
+        response = self.client.get(reverse('patients-list'))
+        self.assertEqual(response.status_code, 403)
+    
+    def test_view_patients_unauthenticated(self):
+        response = self.client.get(reverse('patients-details'))
+        self.assertEqual(response.status_code, 201)    
+     
