82ce9a9d4ad3c0dac7bbaa1284138bf6d99d42bd
emzzy <ezehemmanuel265@gmail.com>
2025-01-06 04:07:47 +0000

    url for register patient form

--- src/teleapp/urls.py
+++ src/teleapp/urls.py
@@ -27,7 +27,9 @@ from .views import (
     home_view, 
     about_view, 
     user_only_view,
-    staff_only_view
+    staff_only_view,
+    doctor_register_view,
+    patient_register_view
 )
 
 urlpatterns = [
@@ -43,6 +45,9 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     #path('api-auth/', include('rest_framework.urls')), # Django rest framework login/logout views
     path('api/', include('api.urls')),
+    path('doctor-register/', doctor_register_view, name='doctor_register'),
+    path('patient-register/', patient_register_view, name='patient_register'),
+    
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
