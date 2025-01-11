feaf0af9fb234e115f21ab2c623d170843639dee
emzzy <ezehemmanuel265@gmail.com>
2025-01-11 01:13:16 +0000

    made changes to admin class

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Patient, MedicalProfessional
 
-admin.site.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'gender', 'location')
+
+admin.site.register(Patient, PatientAdmin)
+
 admin.site.register(MedicalProfessional)
