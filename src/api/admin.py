200c32cb95fe55b3f4baef72a72a9e3befac509f
emzzy <ezehemmanuel265@gmail.com>
2025-01-14 19:59:51 +0000

    added admin views

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Patient, MedicalProfessional
+from django.contrib.auth.admin import UserAdmin
+from .models import Patient, MedicalProfessional, UserAccount
 
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('user', 'phone_number', 'gender', 'location')
 
-admin.site.register(Patient, PatientAdmin)
+class UserAdminConfig(UserAdmin):
+    search_fields = ('email', 'first_name', 'last_name', 'type')
+    ordering = ('-email',)
+    list_display = ('email', 'first_name', 'is_active', 'is_staff', 'type')
 
+
+admin.site.register(UserAccount, UserAdminConfig)
+admin.site.register(Patient)
 admin.site.register(MedicalProfessional)
