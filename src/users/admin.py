74643102ca168e2c52c54b78bf8ea59edbf610f8
emzzy <ezehemmanuel265@gmail.com>
2025-01-15 15:51:54 +0000

    added user creation field in admin view

--- src/users/admin.py
+++ src/users/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .models import Patient, MedicalProfessional, UserAccount
+from .forms import UserAccountChangeForm, UserAccountCreationForm
 
 #User = get_user_model()
 
 class UserAdminConfig(UserAdmin):
+    add_form = UserAccountCreationForm
+    form = UserAccountChangeForm
+    model = UserAccount
     search_fields = ('email', 'first_name', 'last_name', 'type')
     ordering = ('-email',)
     list_display = ('email', 'first_name', 'is_active', 'is_staff', 'type')
+    
 
 
 admin.site.register(UserAccount, UserAdminConfig)
