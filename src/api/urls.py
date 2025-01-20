ab7ec2a0bfc0b00569bc869f96e35db250e01edc
emzzy <ezehemmanuel265@gmail.com>
2025-01-20 19:13:03 +0000

    urls for api views

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PatientSignupView, SelectedRole
+from .views import PatientSignupView, SelectedRole, list_users
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    #path('patients/', views.PatientListAPIView.as_view(), name='patients-list'), # all patients
-    # path('patients/<int:pk>', views.PatientDetailsAPIView.as_view(), name='patient-details'),
-    # path('doctors/', views.MedicalProfessionalListAPIView.as_view(), name='doctors-list'),
-    # path('doctors/<int:pk>', views.MedicalProfessionalDetailsAPIView.as_view(), name='doctor-details'),
     path('patient-register/', PatientSignupView.as_view(), name='patient-register'),
-    path('signup-patient/', views.signup_patient, name='signup-patient'),
-    path('select-role/', SelectedRole.as_view(), name='select-role')
+    path('signup-patient/', views.user_signup, name='signup-patient'),
+    path('select-role/', SelectedRole.as_view(), name='select-role'),
+    path('all-users/', views.list_users, name='all-users')
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
