b2dfa05f1069ce66fd7f7a8917df1de68865f17e
emzzy <ezehemmanuel265@gmail.com>
2025-01-21 01:24:35 +0000

    forms

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,15 +1,29 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import UserAccount
+from django import forms
+from profiles.models import UserAccount
+
 
 class UserAccountCreationForm(UserCreationForm):
      
      class Meta:
             model = UserAccount
-            fields = ['email', 'first_name', 'last_name', 'phone_number', 'gender', 'date_of_birth', 'location']
+            fields = ['email', 'first_name', 'last_name', 'password', 'phone_number', 'gender', 'date_of_birth', 'location']
 
-
+# to modify user's acccount
 class UserAccountChangeForm(UserChangeForm):
      
      class Meta:
             model = UserAccount
             fields = ['email', 'first_name', 'last_name']
+
+# to update patient details after signup
+# class PatientForm(forms.modelfor):
+#       class Meta:
+#             model = Patient
+#             fields = ['location', 'age', 'emergency_contact', 'medical_information']
+
+# to update medical professional details after signup
+# class MedicalProfessionalForm(forms.ModelForm):
+#       class Meta:
+#             model = MedicalProfessional
+#             fields = ['title', 'medical_license', 'specialty', 'years_of_experience', 'professional_certificate']
