ad9223b378fedb89e9e10278ad1aa171a82cfeb9
emzzy <ezehemmanuel265@gmail.com>
2025-01-21 18:33:48 +0000

    stores selected role in sessions

--- src/users/middleware.py
+++ src/users/middleware.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 
 class PatientMiddleware:
     def __init__(self, get_response):
@@ -10,18 +12,18 @@ class PatientMiddleware:
         path = request.path
         user = request.user
 
-        if path.startswith('patients/'):
-            if not user.is_patient:
-                response = JsonResponse({'message': 'you are not authorized to access this page'}, status=HTTPStatus.UNAUTHORIZED)
-
-                return response
+        if path.startswith('patients/') and not user.is_patient:
+            response = JsonResponse({'message': 'you are not authorized to access this page'}, status=HTTPStatus.UNAUTHORIZED)
         
-        if path.startswith('medicals/'):
-            if not user.is_medical_professional:
-                response = JsonResponse({'message': 'you are not authorized to access this page'}, status=HTTPStatus.UNAUTHORIZED)
+        if path.startswith('medicals/') and not user.is_medical_professional:
+            response = JsonResponse({'message': 'you are not authorized to access this page'}, status=HTTPStatus.UNAUTHORIZED)
 
-                return response
-        
+        if path.startswith('/select-role/') and 'type' in request.GET:
+            role_type = request.GET.get('type')
+            if role_type in ['PATIENT', 'MEDICALPROFESSIONAL']:
+                request.session['select-role'] = role_type
+                return redirect(reverse('user-signup/'))
+
         response = self.get_response(request)
 
         return response
