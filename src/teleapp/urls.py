3a0d7197f625798eb1d8198b6d473067ffb4f8e6
emzzy <ezehemmanuel265@gmail.com>
2025-01-21 01:24:00 +0000

    urls

--- src/teleapp/urls.py
+++ src/teleapp/urls.py
@@ -23,20 +23,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import (
-    home_view, 
-    user_only_view,
-    staff_only_view,
-    doctor_register_view,
-    patient_register_view
+    home_view,
+    user_register_view,
+    role_selector
 )
 
 urlpatterns = [
     path("", home_view, name="home"), # index page -> root page
     path('api/', include('api.urls')),
-    path('doctor-signup/', doctor_register_view, name='doctor_signup'),
-    path('patient-signup/', patient_register_view, name='patient_signup'),
+    path('user-signup/', user_register_view, name='user-signup'),
+    path('select-role/', role_selector, name='select-role'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('silk/', include('silk.urls', namespace='silk')), # for api optimization during development
+    path('silk/', include('silk.urls', namespace='silk')) # for api optimization during development
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
