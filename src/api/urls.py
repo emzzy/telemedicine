7942af91e8b0f494dc3680edefd3eb4ccfd688a4
emzzy <ezehemmanuel265@gmail.com>
2025-01-23 23:39:26 +0000

    changes

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from . import views
-from .views import SelectedRole, ListUsersAPIView, ListMedicalProfessionalView, ListMedicalProfessionalView, UserRegistrationView, GetUserView
+from .views import SelectedRole, ListUsersAPIView, ListMedicalProfessionalView, ListMedicalProfessionalView, UserRegistrationView, GetUserView, LoginView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('signup/', UserRegistrationView.as_view(), name='user-signup'),
-    #
-    path('select-role/', SelectedRole.as_view(), name='select-role'),
+    path('user-signup/', UserRegistrationView.as_view(), name='api-user-signup'),
+    path('login/', LoginView.as_view(), name='api-user-login'),
+    path('select-role/', SelectedRole.as_view(), name='api-select-role'),
     path('users/', ListUsersAPIView.as_view(), name='users'), # returns all users from table
     path('users/get-user/<int:pk>', GetUserView.as_view(), name='get-user'), # returns one user with their pk
     path('users/is-patient/', ListMedicalProfessionalView.as_view(), name='is-patient'), # returns only patients from table
