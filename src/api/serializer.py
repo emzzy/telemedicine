0ea211e3720b9df05313fb6f195ae25db9ae57b6
emzzy <ezehemmanuel265@gmail.com>
2025-01-27 18:55:45 +0000

    serializer for custom fields obtain token

--- src/api/serializer.py
+++ src/api/serializer.py
@@ -8,8 +8,9 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
-
+        
         token['email'] = user.email
+
         return token
 
 
