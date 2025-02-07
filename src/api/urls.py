f91171b6e34422c0db516106f769bbfbc5bd7ff8
emzzy <ezehemmanuel265@gmail.com>
2025-02-07 23:05:43 +0000

    email verify view

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
-from .views import (
-    SelectedRole, ListUsersAPIView, ListMedicalProfessionalView, ListMedicalProfessionalView, 
-    UserRegistrationView, GetUserView, LoginAPIView, UserLogoutSerializer
-)
+from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('user-signup/', UserRegistrationView.as_view(), name='api-user-signup'),
-    path('user-login/', LoginAPIView.as_view(), name='api-user-login'),
-    path('user-logout/', UserLogoutSerializer.as_view(), name='api-user-logout'),
-    path('select-role/', SelectedRole.as_view(), name='api-select-role'),
-    path('users/', ListUsersAPIView.as_view(), name='users'), # returns all users from table
-    path('users/get-user/<int:pk>', GetUserView.as_view(), name='get-user'), # returns one user with their pk
-    path('users/is-patient/', ListMedicalProfessionalView.as_view(), name='is-patient'), # returns only patients from table
-    path('users/is-medical-professional/', ListMedicalProfessionalView.as_view(), name='is-medical-professional'), # returns only med prof from table
+    path('user-signup/', views.UserRegistrationView.as_view(), name='api-user-signup'),
+    path('user-login/', views.LoginAPIView.as_view(), name='api-user-login'),
+    path('user-logout/', views.UserLogoutSerializer, name='api-user-logout'),
+    path('verify-email/', views.VerifyEmail.as_view(), name='api-verify-email'),
+    path('select-role/', views.SelectedRole.as_view(), name='api-select-role'),
+    path('users/', views.ListUsersAPIView.as_view(), name='users'), # returns all users from table
+    path('users/get-user/<int:pk>', views.GetUserView.as_view(), name='get-user'), # returns one user with their pk
+    path('users/patients/', views.ListPatientView.as_view(), name='is-patient'), # returns only patients from table
+    path('users/medical-professionals/', views.ListMedicalProfessionalView.as_view(), name='is-medical-professional'), # returns only med prof from table
+    path('users/delete-user/<int:pk>', views.DeleteUserAccount.as_view(), name='delete-user')
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
