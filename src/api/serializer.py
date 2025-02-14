047f33b6d89b3a6b2cfe01059b3810eb01a0ec6e
emzzy <ezehemmanuel265@gmail.com>
2025-02-14 02:20:52 +0000

    logout serializer

--- src/api/serializer.py
+++ src/api/serializer.py
@@ -101,3 +101,19 @@ class EmailVerificationSerializer(serializers.ModelSerializer):
         model = UserAccount
         fields = ['token']
         
+class UserLogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    default_error_messages = {
+        'bad_token': ('Token is expired or invalid')
+    }
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+    
+    def save(self, **kwargs):
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            self.fail('bad_token')
