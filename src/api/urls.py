a4e84be3c72dc851db134f0153de4008cb0148b9
emzzy <ezehemmanuel265@gmail.com>
2025-02-18 16:02:02 +0000

    reset password url

--- src/api/urls.py
+++ src/api/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('user-login/', views.LoginAPIView.as_view(), name='api-user-login'),
     path('user-logout/', views.LogoutAPIView.as_view(), name='api-user-logout'),
     path('verify-email/', views.VerifyEmail.as_view(), name='api-verify-email'),
+    path('password-reset/<uidb64>/token/', views.PAsswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+
     path('select-role/', views.SelectedRole.as_view(), name='api-select-role'),
     path('users/', views.ListUsersAPIView.as_view(), name='users'), # returns all users from table
     path('users/get-user/<int:pk>', views.GetUserView.as_view(), name='get-user'), # returns one user with their pk
