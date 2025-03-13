c91c292dca7241029543b0df3b511e4c0769e276
emzzy <ezehemmanuel265@gmail.com>
2025-03-13 13:43:23 +0000

    Notification Type for Doctor and Patient Models

--- src/patient/models.py
+++ src/patient/models.py
@@ -28,7 +28,7 @@ class Patient(models.Model):
 class Notification(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True, blank=True)
     appointment = models.ForeignKey('base.Appointment', on_delete=models.CASCADE, null=True, blank=True, related_name='patients_appointment_notification')
-    type = models.CharField(max_length=100, choices=None)
+    type = models.CharField(max_length=100, choices=NOTIFICATION_TYPE)
     seen = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
 
