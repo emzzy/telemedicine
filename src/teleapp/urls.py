b16335e162f4ab29b132f345dddbeafedcd0ea16
emzzy <ezehemmanuel265@gmail.com>
2025-03-13 01:45:14 +0000

    media url

--- src/teleapp/urls.py
+++ src/teleapp/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import home_view, user_register_view, user_login, role_selector, patient_dashboard, med_pro_dashboard, user_logout
 
-from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -40,6 +39,6 @@ urlpatterns = [
     path('silk/', include('silk.urls', namespace='silk')), # for api optimization during development
     # swagger-UI
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    #
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
