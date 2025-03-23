7038eb63e03f106b0a3d3eb6ddc1ada203fa7260
emzzy <ezehemmanuel265@gmail.com>
2025-03-23 12:26:41 +0000

    Removed duplicated files from root Directory

--- src/patient/managers.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import BaseUserManager
-from users.models import UserAccount
-
-class PatientManager(BaseUserManager):
-    def get_queryset(self, *args, **kwargs):
-        results = super().get_queryset(*args, **kwargs)
-        return results.filter(role=UserAccount.Role.PATIENT)
