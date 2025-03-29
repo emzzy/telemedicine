c98e2c4d636e4438e0bad9a86b3f356e9474af3c
emzzy <ezehemmanuel265@gmail.com>
2025-03-29 23:56:52 +0000

    Base App URL

--- backend/src/teleapp/urls.py
+++ backend/src/teleapp/urls.py
@@ -23,22 +23,22 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("", home_view, name="home"),
-    path('agora/', include('agora.urls')),
-    path('api/', include('api.urls')),
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-    path('signup/', user_register_view, name='signup'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('select-role/', role_selector, name='select-role'),
-    path('patient-dashboard/', patient_dashboard, name='patient-dashboard'),
-    path('medical-professional-dashboard/', med_pro_dashboard, name='medical-professional-dashboard'),
-    path('silk/', include('silk.urls', namespace='silk')), # for api optimization during development
-    # swagger-UI
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+   path("", home_view, name="home"),
+   path('agora/', include('agora.urls')),
+   path('api/', include('api.urls')),
+   path('admin/', admin.site.urls),
+   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+   path('signup/', user_register_view, name='signup'),
+   path('login/', user_login, name='login'),
+   path('logout/', user_logout, name='logout'),
+   path('select-role/', role_selector, name='select-role'),
+   path('patient-dashboard/', patient_dashboard, name='patient-dashboard'),
+   path('medical-professional-dashboard/', med_pro_dashboard, name='medical-professional-dashboard'),
+   path('silk/', include('silk.urls', namespace='silk')), # for api optimization during development
+   # swagger-UI
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
