1851524ee2953dd79fffcccdae8e5f323ce3362c
emzzy <ezehemmanuel265@gmail.com>
2025-04-01 11:08:39 +0100

    API URL

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
-from  rest_framework.routers import DefaultRouter
-from doctor.views import views as doctor_views
-from patient.views import views as patient_views
+from base.views import service_detail
+#from  rest_framework.routers import DefaultRouter
+# from doctor.views import views as doctor_views
+# from patient.views import views as patient_views
 
-router = DefaultRouter()
-router.register(r'doctor', doctor_views)
-router.register(r'patient', patient_views)
+# router = DefaultRouter()
+# router.register(r'doctor', doctor_views)
+# router.register(r'patient', patient_views)
 
 urlpatterns = [
+    # Auth views
     path('user-signup/', views.UserRegistrationView.as_view(), name='api-user-signup'),
     path('user-login/', views.LoginAPIView.as_view(), name='api-user-login'),
     path('user-logout/', views.LogoutAPIView.as_view(), name='api-user-logout'),
@@ -19,10 +21,19 @@ urlpatterns = [
     path('password-reset-complete/', views.SetNewPassword.as_view(), name='password-reset-complete'),
     path('select-role/', views.SelectedRole.as_view(), name='api-select-role'),
     
+    # All Users
     path('users/', views.ListUsersAPIView.as_view(), name='users'), # returns all users from table
     path('users/get-user/<int:pk>', views.GetUserView.as_view(), name='get-user'), # returns one user with their pk
     path('users/patients/', views.ListPatientView.as_view(), name='is-patient'), # returns only patients from table
     path('users/medical-professionals/', views.ListMedicalProfessionalView.as_view(), name='is-medical-professional'), # returns only med prof from table
-    path('users/delete-user/<int:pk>', views.DeleteUserAccount.as_view(), name='delete-user')
+    path('users/delete-user/<int:pk>', views.DeleteUserAccount.as_view(), name='delete-user'),
+
+    # Patients
+    
+    # Doctors
+
+    # Services
+    #path('service/add-service/', name='add-a-service'),
+    path('service/<service_id>/', service_detail, name='service_detail') 
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
