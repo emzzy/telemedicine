15fd453708d05495cc79c94f3cf6b42e778aa0c8
emzzy <ezehemmanuel265@gmail.com>
2025-04-05 23:39:08 +0100

    Changes

--- backend/src/base/views.py
+++ backend/src/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from base import models as base_models
 from django.contrib.auth.decorators import login_required
 from doctor import models as doctor_model
@@ -12,7 +12,49 @@ def book_appointment(request, service_id, doctor_id):
     doctor = doctor_model.MedicalProfessional.objects.get(id=doctor_id)
     patient = patient_model.Patient.objects.get(id=service_id)
 
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+        mobile = request.POST.get('mobile')
+        gender = request.POST.get('mobile')
+        address = request.POST.get('first_name')
+        dob = request.POST.get('dob')
+        issues = request.POST.get('issues')
+        symptoms = request.POST.get('symptoms')
+
+        # Update patient bio data
+        patient.first_name = first_name
+        patient.email = email
+        patient.mobile = mobile
+        patient.gender = gender
+        patient.address = address
+        patient.dob = dob
+        patient.save()
+        
+        # Appointment obj
+        appointment = base_models.Appointment.objects.create(
+            service = service,
+            doctor=doctor,
+            patient=patient,
+            new_appointment_date=doctor.available_appointment_date,
+            issues=issues,
+            symptoms=symptoms
+        )
+        # Billing object
+        billing = base_models.Billing()
+        billing.patient = patient,
+        billing.appointment = appointment,
+        billing.sub_total = appointment.service.cost,
+        billing.tax = appointment.service.cost * 20/100,
+        billing.total = billing.sub_total + billing.tax
+        billing.status = 'Unpaid'
+
+        return redirect('checkout', billing.billing_id)
+    
     context = {
-        'service': service
+        'service': service,
+        'patient': patient,
+        'doctor': doctor
     }
     return render(request, 'book_appointment.html', context)
+
