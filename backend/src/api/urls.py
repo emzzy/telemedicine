c69fdfd8b04b6e58f1d45214410e3fd54bcc98b1
emzzy <ezehemmanuel265@gmail.com>
2025-04-24 23:21:11 +0100

    API urls

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -12,9 +12,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     # Auth views
-    path('user-signup/', views.UserRegistrationView.as_view(), name='api-user-signup'),
-    path('user-login/', views.LoginAPIView.as_view(), name='api-user-login'),
-    path('user-logout/', views.LogoutAPIView.as_view(), name='api-user-logout'),
+    path('user/signup/', views.UserRegistrationView.as_view(), name='api-user-signup'),
+    path('user/login/', views.LoginAPIView.as_view(), name='api-user-login'),
+    path('user/logout/', views.LogoutAPIView.as_view(), name='api-user-logout'),
     path('verify-email/', views.VerifyEmail.as_view(), name='api-verify-email'),
     path('request-reset-email/', views.RequestPasswordResetEmail.as_view(), name='request-reset-email'),
     path('password-reset/<uidb64>/<token>/', views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
@@ -26,6 +26,7 @@ urlpatterns = [
     path('users/get-user/<int:pk>', views.GetUserView.as_view(), name='get-user'), # returns one user with their pk
     path('users/patients/', views.ListPatientView.as_view(), name='is-patient'), # returns only patients from table
     path('users/medical-professionals/', views.ListMedicalProfessionalView.as_view(), name='is-medical-professional'), # returns only med prof from table
+    path('users/all-doctors/', views.ListDoctorsView.as_view(), name='list-doctors'),
     path('users/delete-user/<int:pk>', views.DeleteUserAccount.as_view(), name='delete-user'),
 
     # Patients
