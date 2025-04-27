5ca0d04b215e29360487b6815af75e7d2927f2d1
emzzy <ezehemmanuel265@gmail.com>
2025-04-27 22:08:17 +0100

    Settings file

--- backend/src/teleapp/settings.py
+++ backend/src/teleapp/settings.py
@@ -67,7 +67,8 @@ ALLOWED_HOSTS = []
 if DEBUG:
     ALLOWED_HOSTS += [
         "127.0.0.1",
-        "localhost"
+        "localhost",
+        "http://localhost:5173/"
     ]
 
 # Application definition
