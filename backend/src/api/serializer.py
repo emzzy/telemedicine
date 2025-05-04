65fae091cea620a2b1f8318c4e74fc53475b549e
emzzy <ezehemmanuel265@gmail.com>
2025-05-04 01:59:49 +0100

    API Serializer

--- backend/src/api/serializer.py
+++ backend/src/api/serializer.py
@@ -43,15 +43,15 @@ class DoctorListSerializer(serializers.ModelSerializer):
 
 
 class UserAccountSerializer(serializers.ModelSerializer):
-    patient = PatientSerializer(read_only=True)
-    medical_professional = MedicalProfessionalSerializer(read_only=True)
+    #patient = PatientSerializer(read_only=True)
+    #medical_professional = MedicalProfessionalSerializer(read_only=True)
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = UserAccount
         fields = (
             'id', 'email', 'first_name', 'last_name', 'password', 'phone_number', 'gender', 'date_of_birth', 'location',
-            'is_patient', 'is_medical_professional', 'patient', 'medical_professional'
+            'is_patient', 'is_medical_professional'
         )
 
 
