9e1a7071f34c417b11e2a4c351d971f326f7221b
emzzy <ezehemmanuel265@gmail.com>
2025-05-04 02:00:49 +0100

    Available doctors erializer

--- backend/src/base/serializers.py
+++ backend/src/base/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-from .models import *
+from .models import Service, Appointment, MedicalRecord, LabTest, Prescription, Billing
 
-class ServiceSerializer(serializers.ModelSerializer):
+
+class ServicesListSerializer(serializers.ModelSerializer):
+    from api.serializer import UserAccountSerializer
+    doctor_count = serializers.SerializerMethodField()
+    available_doctors = UserAccountSerializer(many=True)
+
     class Meta:
         model = Service
-        fields = []
+        fields = ['id', 'name', 'description', 'cost', 'doctor_count', 'image', 'available_doctors']
 
+    def get_doctor_count(self, obj):
+        return obj.available_doctors.count()
+    
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
