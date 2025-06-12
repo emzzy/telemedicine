cc9c90719fa13484e965ef246538acad3d4cf493
emzzy <ezehemmanuel265@gmail.com>
2025-06-12 23:50:41 +0100

    Default User and Doctor Serializers

---
+++ backend/src/shared/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from doctor.models import MedicalProfessional
+from users.models import UserAccount
+
+
+
+class UserAccountSerializer(serializers.ModelSerializer):
+    #patient = PatientSerializer(read_only=True)
+    #medical_professional = MedicalProfessionalSerializer(read_only=True)
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = UserAccount
+        fields = (
+            'id', 'email', 'first_name', 'last_name', 'password', 'phone_number', 'gender', 'date_of_birth', 'location',
+            'is_patient', 'is_medical_professional'
+        )
+
+
+class MedicalProfessionalsSerializer(serializers.ModelSerializer):
+    user = UserAccountSerializer()
+
+    class Meta:
+        model = MedicalProfessional
+        fields = [
+            'id', 'user', 'title', 'image', 'bio', 'medical_license', 'specialty', 'years_of_experience',
+            'professional_certificate', 'available_appointment_date'
+    ]
