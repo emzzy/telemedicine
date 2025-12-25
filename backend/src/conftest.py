c73664d852c828efc7c5d0affd474ce6c7015d1b
emzzy <ezehemmanuel265@gmail.com>
2025-12-25 23:46:15 +0000

    Pytest config

--- backend/src/conftest.py
+++ backend/src/conftest.py
@@ -9,39 +9,37 @@ from doctor.models import MedicalProfessional
 
 User = get_user_model()
 
-register(UserAccount)
+#register(UserAccount)
 
+
+# @pytest.fixture
+# def new_user(db, user_factory):
+#     user = user_factory.create()
+#     return user
+
+
+# @pytest.fixture
+# def user_data():
+#     return {
+#         "email": "eazy@gmail.com",
+#         "first_name": "Doe",
+#         "last_name": "John",
+#         "password": "securepassword",
+#         "confirm_password": "securepassword",
+#         "phone_number": "123456789",
+#         "gender": "male",
+#         "date_of_birth": "1990-01-01",
+#         "location": "City Name",
+#         "role": "medical_professional",
+#     }
 
 @pytest.fixture
-def new_user(db, user_factory):
-    user = user_factory.create()
-    return user
-
-
-@pytest.fixture
-def user_data():
-    return {
-        "email": "eazy@gmail.com",
-        "first_name": "Doe",
-        "last_name": "John",
-        "password": "securepassword",
-        "confirm_password": "securepassword",
-        "phone_number": "123456789",
-        "gender": "male",
-        "date_of_birth": "1990-01-01",
-        "location": "City Name",
-        "role": "medical_professional",
-    }
-
-@pytest.fixture
-def create_user(db, user_data):
-    user = user_data.create()
-    return UserAccount.objects.create(
-        email="eazy@gmail.com",
+def create_user(db):
+    user = UserAccount.objects.create(
+        email="test@mail.com",
         first_name="Doe",
-        last_name="John"
+        last_name="John",
         password="securepassword",
-        
         confirm_password="securepassword",
         phone_number="123456789",
         gender="male",
@@ -49,7 +47,27 @@ def create_user(db, user_data):
         location="City Name",
         role="medical_professional",
     )
+    return user
 
 @pytest.fixture
-def user_patient(db, create_user):
-    pass
+def new_user_factory(db, create_user):
+    def create_app_user(
+        email: str = "test@mail.com",
+        first_name: str = "Doe",
+        last_name: str = "John",
+        password: str = None,
+        confirm_password: str = "securepassword",
+        phone_number: str = "123456789",
+        gender: str = "male",
+        date_of_birth: str = "1990-01-01",
+        location: str = "City Name",
+        role: str = "medical_professional",
+        is_active: str = True,
+        is_superuser: str = False,
+        is_staff: str = False,
+    )
+    return create_patient_user
+
+@pytest.fixture
+def user_A(db, new_user_factory)
+    
