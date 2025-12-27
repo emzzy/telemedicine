87cae1f33ce1170b9cb2d857ae01cf1636c2f731
emzzy <ezehemmanuel265@gmail.com>
2025-12-27 09:36:09 +0000

    Added tests for UserAccount model

--- backend/src/users/tests/test_models.py
+++ backend/src/users/tests/test_models.py
@@ -1,9 +1,26 @@
+from django.db import IntegrityError
 import pytest
+from django.contrib.auth import get_user_model, authenticate
+
+#from users.tests.factories import UserAccountFactory
+from users.models import UserAccount
+
+User = get_user_model()
+
+@pytest.mark.skip
+def test_get_email(new_user1):
+    pass
 
 
-def test_new_user(new_user1):
-    assert new_user1.email == 'test@example.com'
+def test_created_user_email(new_user1):
+    print(new_user1.email)
 
+    assert '@' in new_user1.email
+    assert new_user1.email is not None
+    assert new_user1.email != ''
 
-def test_new_staff(new_user2):
-    assert new_user2.is_staff
+def test_email_is_unique(new_user1, user_account_factory):
+    print(new_user1.email)
+
+    with pytest.raises(IntegrityError):
+        user_account_factory(email=new_user1.email)
