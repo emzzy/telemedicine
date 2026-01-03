28c1c6e360773abe597a57ce1662685759ac4f0c
emzzy <ezehemmanuel265@gmail.com>
2026-01-03 23:35:06 +0000

    Added tests for service model

--- backend/src/base/tests/test_models.py
+++ backend/src/base/tests/test_models.py
@@ -1,5 +1,7 @@
 import pytest
 
+from doctor.tests.factories import MedicalProfessionalFactory
+from users.tests.factories import UserAccountFactory
 from base.models import Service
 
 @pytest.mark.skip
@@ -9,23 +11,23 @@ def test_appointment(appointment_factory):
     assert True
 
 
+@pytest.mark.django_db
 @pytest.mark.parametrize(
-    'image, name, description, cost, available_doctors, doctor_details',
+    'image, name, description, cost',
     [
-        ('service_image_url', 'service_name', 'this service offfers solution to treat teeth problems' , 50.0, [1], '1'),
-        # ('service_image_url', 'service_name', 'fix your tooth', 1, 'dentist'),
+        ('service_image_url', 'service_name', 'this service offfers solution to treat teeth problems' , 50.0),
     ],
 )
-def test_service_instance(
-    db, service_factory, image, name, description, cost, available_doctors, doctor_details
-):
-    test = service_factory(
+def test_service_instance(db, service_factory, image, name, description, cost):
+    doctors_list = UserAccountFactory(is_medical_professional=True)
+    doctor_description = MedicalProfessionalFactory()
+
+    service = service_factory(
         image=image,
         name=name,
         description=description,
         cost=cost,
-        available_doctors=available_doctors,
-        doctor_details=doctor_details
+        available_doctors=[doctors_list],
+        doctor_details=[doctor_description],
     )
-    test = Service.objects.all().count()
-    assert test == True
+    assert Service.objects.all().count() == 1
