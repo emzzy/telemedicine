ac8e75b361a6c43f5e780080158ecb19d9aefb22
emzzy <ezehemmanuel265@gmail.com>
2026-01-06 23:47:50 +0000

    Initial config

--- backend/src/config/django/local.py
+++ backend/src/config/django/local.py
@@ -1,2 +1,35 @@
 from .base import *
 
+DEBUG = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost",
+        "http://localhost:5173/"
+    ]
+
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+        'OPTIONS': {
+        },
+    },
+
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    # BASE_DIR / 'frontend' / 'static',
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles' # for prod
+
+
+MEDIA_URL = '/media/'
