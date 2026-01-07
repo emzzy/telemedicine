5ec3fe5bda777021890ae501f8ae0d122e54e32c
emzzy <ezehemmanuel265@gmail.com>
2026-01-07 23:59:27 +0000

    Factories

--- backend/src/users/tests/factories.py
+++ backend/src/users/tests/factories.py
@@ -9,8 +9,18 @@ class UserAccountFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = UserAccount
         
-    email = fake.email()
-    first_name = fake.first_name()
-    last_name = fake.last_name()
-    password = fake.password()
-    is_staff = 'True'
+    email = factory.Faker('email') # pyright: ignore[reportPrivateImportUsage]
+    first_name = factory.Faker('first_name') # type: ignore
+    last_name = factory.Faker('last_name') # type: ignore
+    password = factory.PostGenerationMethodCall('set_password', 'Password1234') # type: ignore
+    phone_number = '07123456789'
+    gender = factory.Iterator(['Male', 'Female', 'Other', 'Prefer not to say']) # type: ignore
+    date_of_birth = factory.Faker('date_of_birth', minimum_age=18, maximum_age=95) # type: ignore
+
+    is_active = True
+    is_admin = False
+    is_staff = False
+    is_superuser = False
+    is_verified = False
+    is_patient = False
+    is_medical_professional = False
