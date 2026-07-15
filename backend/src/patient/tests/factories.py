ac62738cdecffc34b2b9135529dbce1393565d8b
emzzy <ezehemmanuel265@gmail.com>
2026-07-15 19:02:09 +0100

    refactor(tests): modify patient test factory

--- backend/src/patient/tests/factories.py
+++ backend/src/patient/tests/factories.py
@@ -1,8 +1,6 @@
 import factory
 from faker import Faker
 
-from base.tests.factories import AppointmentFactory
-from users.tests.factories import UserAccountFactory
 from patient.models import Notification, Patient
 
 fake = Faker()
@@ -11,21 +9,27 @@ class PatientFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Patient
 
-    user = factory.SubFactory(UserAccountFactory)
+    user = factory.SubFactory('users.tests.factories.UserAccountFactory') # pyright: ignore[reportPrivateImportUsage]
     image = fake.image()
     image_caption = fake.catch_phrase()
     age = fake.date_of_birth()
-    emergency_contact = fake.mobile_number()
+    emergency_contact = '123456789'
     medical_information = fake.texts()
-    blood_group = fake.vehicle_category_letter()
+    blood_group = 'AA'
 
 
-class NotificationFactory(factory.django.DjangoModelFactory):
+class PatientNotificationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Notification
 
-    patient = factory.Subfactory(PatientFactory)
-    appointment = factory.Subfactory(AppointmentFactory)
+    patient = factory.SubFactory(PatientFactory) # pyright: ignore[reportPrivateImportUsage]
+
+    @factory.lazy_attribute # pyright: ignore[reportPrivateImportUsage]
+    def appointment(self):
+        from base.tests.factories import AppointmentFactory
+        return AppointmentFactory()
+    
+    # appointment = factory.SubFactory(AppointmentFactory) # pyright: ignore[reportPrivateImportUsage]
     type = fake.name()
     seen = fake.boolean()
     date = fake.date_time()
