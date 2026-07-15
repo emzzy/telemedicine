5213c02266c2262573d65548acb1b7fdf4b4cd66
emzzy <ezehemmanuel265@gmail.com>
2026-07-15 19:16:20 +0100

    refactor(tests): modify doctor test factory

--- backend/src/doctor/tests/factories.py
+++ backend/src/doctor/tests/factories.py
@@ -1,36 +1,48 @@
 import factory
 from faker import Faker
 
-from base.tests.factories import AppointmentFactory
 from doctor.models import MedicalProfessional, Notification
-from django.contrib.auth import get_user_model
 from users.tests.factories import UserAccountFactory
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-User = get_user_model()
 fake = Faker()
 
 class MedicalProfessionalFactory(factory.django.DjangoModelFactory):
     class Meta:
-        models = MedicalProfessional
+        model = MedicalProfessional
 
-    user = factory.SubFactory(UserAccountFactory)
-    title = fake.name
-    image = fake.image()
-    image_caption = fake.catch_phrase_verb()
-    bio = fake.texts()
-    medical_license = fake.license_plate()
-    specialty = fake.text()
-    years_of_experience = fake.house_number()
-    professional_certificate = fake.file_extension()
-    available_appointment_date = fake.date_time_ad
+    user = factory.SubFactory(UserAccountFactory) # pyright: ignore[reportPrivateImportUsage]
+    title = fake.prefix()
+    image = factory.django.ImageField(
+        filename='doctor.jpg', width=100, height=100, color='blue'
+    )
+
+    image_caption = 'test image caption'
+    bio = fake.text(max_nb_chars=200)
+    
+    medical_license = '7854123652'
+
+    specialty = 'doctor'
+    years_of_experience = 5
+    
+    professional_certificate = factory.django.FileField(
+        filename='certificate.pdf', data=b'fake certificate content'
+    )
+    
+    available_appointment_date = None
 
 
-class NotificationFactory(factory.django.DjangoModelFactory):
+class DoctorNotificationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Notification
 
-    doctor = factory.Subfactory(MedicalProfessionalFactory)
-    appointment = factory.Subfactory(AppointmentFactory)
-    type = fake.name()
-    seen = fake.boolean()
-    date = fake.date_time()
+    doctor = factory.SubFactory(MedicalProfessionalFactory) # pyright: ignore[reportPrivateImportUsage]
+
+    @factory.lazy_attribute # pyright: ignore[reportPrivateImportUsage]
+    def appointment(self):
+        from base.tests.factories import AppointmentFactory
+        return AppointmentFactory()
+    
+    type = factory.Faker('word')
+    seen = factory.Faker('boolean')
+    date = factory.Faker('date_time')
